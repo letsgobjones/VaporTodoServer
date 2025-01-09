3f52334d854263b51c3492e54354265a1f1d8014
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-01-10 08:01:26 +0900

    Implemented TodoDTO to improve data transfer efficiency.
    Updates the `TodoDTO` struct to simplify its structure and improve data transfer efficiency.

--- Sources/App/DTOs/TodoDTO.swift
+++ Sources/App/DTOs/TodoDTO.swift
@@ -15,3 +15,28 @@ struct TodoDTO: Content {
         return model
     }
 }
+
+
+//import Fluent
+//import Vapor
+//
+//struct TodoDTO: Content {
+//  var id: UUID?
+//  var title: String?
+//  var isCompleted: Bool?
+//  var priority: Int?// Add isCompleted
+//  
+//  func toModel() -> Todo {
+//    let model = Todo()
+//    
+//    model.id = self.id
+//    if let title = self.title {
+//      model.title = title
+//    }
+//    if let isCompleted = self.isCompleted {
+//      model.isCompleted = isCompleted
+//    }
+//    model.priority = self.priority // Set priority
+//    return model
+//  }
+//}
