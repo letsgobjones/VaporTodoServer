aa91a93b90043c5e7146ea2f9635c42dea07f689
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-01-10 07:59:49 +0900

    Implemented migration to create todos table.

--- Sources/App/Migrations/CreateTodo.swift
+++ Sources/App/Migrations/CreateTodo.swift
@@ -1,14 +1,19 @@
 import Fluent
 
 struct CreateTodo: AsyncMigration {
-    func prepare(on database: Database) async throws {
-        try await database.schema("todos")
-            .id()
-            .field("title", .string, .required)
-            .create()
-    }
-
-    func revert(on database: Database) async throws {
-        try await database.schema("todos").delete()
-    }
+  func prepare(on database: Database) async throws {
+    try await database.schema("todos")
+      .id()
+      .field("title", .string, .required)
+      .field("isCompleted", .bool, .required)
+//      .field("dueDate", .datetime, .required)
+      .field("priority", .int, .required)
+//      .field("createdAt", .datetime)
+//      .field("updatedAt", .datetime)
+      .create()
+  }
+  
+  func revert(on database: Database) async throws {
+    try await database.schema("todos").delete()
+  }
 }
